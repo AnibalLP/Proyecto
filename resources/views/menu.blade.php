3577a79e3f2ebd427de89a1902e8dc0a3ad4d6d0
Eunice Cisneros <eucisnerosyt@gmail.com>
2021-04-29 18:15:19 -0600

    Mision y vision

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -12,11 +12,82 @@
 <nav class="navbar navbar-light bg-main">
         <div class="container p-4">
             <a class="navbar-brand m-auto" href="#">
-                <img src="{{asset('images/logo.png')}}" width="120" alt="" loading="lazy">
+                <img src="{{asset('images/LOGO.png')}}" width="200" alt="" loading="lazy">
             </a>
         </div>
     </nav>
+    <section class="container-fluid content">
+        <div class="row justify-content-center">
+            <div class="col-10">
+                <div class="row">
+                    <!-- Post 1 -->
+                    <div class="col-md-4 col-12 justify-content-center mb-5">
+                        <div class="card m-auto" style="width: 18rem;">
+                            <img class="card-img-top" src="{{asset('images/MISION.jpg')}}" alt="Post MISION">
+                            <div class="card-body">
+                                <h5 class="card-title my-2">Misión</h5>
+                                <div class="d-card-text">
+                                Nuestra misión es llegar a alcanzar 
+                                cada uno de nuestros objetivos, poder 
+                                llegar a hacer buen equipo multifuncional,
+                                 ofrecer un buen trabajo, buenos argumentos, 
+                                 buenas ideas, obtener conocimiento de la 
+                                 materia, poner en practica lo aprendido y 
+                                 trabajar en equipo para poder desarrollar 
+                                 las diferentes actividades.
+                                </div>
+                               
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Post 2 -->
+                    <div class="col-md-4 col-12 justify-content-center mb-5">
+                        <div class="card m-auto" style="width: 18rem;">
+                            <img class="card-img-top" src="{{asset('images/VISION.jpg')}}" alt="Post VISION">
+                            <div class="card-body">
+                                <h5 class="card-title my-2">Visión</h5>
+                                <div class="d-card-text">
+                                Nuestra visión es desarrollar una web app
+                                 para que las personas que se 
+                                ven afectadas con el autismo puedan 
+                                informarse de la enfermedad y de esta 
+                                manera ayudarles a solucionar problemas 
+                                cotidianos que se les presentan, orientarlos
+                                 para facilitar sus vidas de una forma gratis y fácil.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Post 3 -->
+                    <div class="col-md-4 col-12 justify-content-center mb-5">
+                        <div class="card m-auto" style="width: 18rem;">
+                            <img class="card-img-top" src="{{asset('images/VYP.jpg')}}" alt="Post VALORES Y PRINCIPIOS">
+                            <div class="card-body">
+                                <h5 class="card-title my-2">Valores y Principios</h5>
+                                <div class="d-card-text">
+                                Confianza - Colaboración - Transparente - Valentía - Respeto al compañero - Actitud positiva - Tiempo – Comunicación.
+                                <hr>
+                                Satisfacer al cliente mediante la entrega temprana y continua de software.
+                                <hr>
+                                Individuos motivados. 
+                                <hr>
+                                Comunicación entre los miembros del equipo, cliente y otros interesados.
+                                <hr>
+                                Ser responsables al momento de cumplir con lo que el cliente solicita el tiempo adecuado.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="col-12">
+                <!-- Paginador -->
+
+            </div>
+        </div>
+    </section>
+ 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
 </body>
